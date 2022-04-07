3d6078cdaa1fb6f8040e70e14db93135bf451d0c
liyang <liyang@datapipeline.com>
2022-04-07 22:15:03 +0800

    fix

--- NetworkFlowAnalysis/src/main/java/com/datapipeline/HotPages.java
+++ NetworkFlowAnalysis/src/main/java/com/datapipeline/HotPages.java
@@ -7,11 +7,15 @@ import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.state.ListState;
 import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
@@ -19,6 +23,7 @@ import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 import javax.xml.crypto.Data;
 import java.net.URL;
@@ -26,14 +31,18 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class HotPages {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment senv = StreamExecutionEnvironment.getExecutionEnvironment();
+        senv.setParallelism(1);
         senv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         URL resource = HotPages.class.getResource("/apache.log");
-        DataStream<String> inputStream = senv.readTextFile(resource.toString());
+//        DataStream<String> inputStream = senv.readTextFile(resource.toString());
+        DataStreamSource<String> inputStream = senv.socketTextStream("localhost", 7777);
+        inputStream.print("data");
         DataStream<ApacheLogEvent> mapStream = inputStream.map(new MapFunction<String, ApacheLogEvent>() {
             @Override
             public ApacheLogEvent map(String line) throws Exception {
@@ -42,21 +51,28 @@ public class HotPages {
                 Long timestamp = simpleDateFormat.parse(fields[3]).getTime();
                 return new ApacheLogEvent(fields[0], fields[1], timestamp, fields[5], fields[6]);
             }
-        }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<ApacheLogEvent>(Time.minutes(1)) {
+        }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<ApacheLogEvent>(Time.seconds(1)) {
             @Override
             public long extractTimestamp(ApacheLogEvent apacheLogEvent) {
                 return apacheLogEvent.getTimestamp();
             }
         });
 
-        DataStream<PageViewCount> windAggreStream = mapStream.filter(line->"GET".equals(line.getMethod())).
+        OutputTag<ApacheLogEvent> outputTag = new OutputTag<ApacheLogEvent>("late"){};
+        SingleOutputStreamOperator<PageViewCount> windAggreStream = mapStream.filter(line -> "GET".equals(line.getMethod())).
                 keyBy(ApacheLogEvent::getUrl).
-                timeWindow(Time.of(10, TimeUnit.MINUTES),Time.of(5,TimeUnit.SECONDS)).
-                aggregate(new HotPageAggregate(),new HotPageWindowFunction());
+                timeWindow(Time.of(10, TimeUnit.MINUTES), Time.of(5, TimeUnit.SECONDS)).
+                allowedLateness(Time.minutes(1)).
+                sideOutputLateData(outputTag).
+                aggregate(new HotPageAggregate(), new HotPageWindowFunction());
+
+        windAggreStream.print("result");
+         windAggreStream.getSideOutput(outputTag).print("lates");
 
         DataStream<String> topN  = windAggreStream.keyBy(PageViewCount::getWindowEnd).process(new HotPageTopN(3));
-//
+
         topN.print();
+
         senv.execute("HotPages");
     }
 
@@ -97,30 +113,42 @@ public class HotPages {
             this.topN = topN;
         }
 
-        private transient ListState<PageViewCount> listState;
+//        private transient ListState<PageViewCount> listState;
+        private transient MapState<String,Long> mapState;
 
         @Override
         public void open(Configuration parameters) throws Exception {
-            listState =   getRuntimeContext().getListState(new ListStateDescriptor<PageViewCount>("listState", TypeInformation.of(PageViewCount.class)));
+//            listState =   getRuntimeContext().getListState(new ListStateDescriptor<PageViewCount>("listState", TypeInformation.of(PageViewCount.class)));
+            mapState = getRuntimeContext().getMapState(new MapStateDescriptor<String, Long>("mapState",String.class,Long.class));
         }
 
 
 
         @Override
         public void processElement(PageViewCount pageViewCount, KeyedProcessFunction<Long, PageViewCount, String>.Context context, Collector<String> collector) throws Exception {
-            listState.add(pageViewCount);
+
+//            listState.add(pageViewCount);
+            mapState.put(pageViewCount.getUrl(),pageViewCount.getCount());
             context.timerService().registerEventTimeTimer(pageViewCount.getWindowEnd()+1);
+            // 这个定时器主要是为了一分钟后清空状态的（因为上面设置了允许数据迟到1分钟）
+            context.timerService().registerEventTimeTimer(pageViewCount.getWindowEnd()+60*1000L);
         }
 
         @Override
         public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
-          ArrayList<PageViewCount> pageViewCounts = Lists.newArrayList(listState.get());
-            pageViewCounts.sort(new Comparator<PageViewCount>() {
+            // 因为我们在上面注册了两个定时器，所以对定时器的触发时机要进行控制
+            if(timestamp == ctx.getCurrentKey()+60*1000L){
+                mapState.clear();
+                return;
+            }
+
+            ArrayList<Map.Entry<String, Long>> pageViewCounts = Lists.newArrayList(mapState.entries());
+            pageViewCounts.sort(new Comparator<Map.Entry<String, Long>>() {
                 @Override
-                public int compare(PageViewCount o1, PageViewCount o2) {
-                    if(o1.getCount()>o2.getCount()){
+                public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
+                    if(o1.getValue()>o2.getValue()){
                         return -1;
-                    }else if(o1.getCount()<o2.getCount()){
+                    }else if(o1.getValue()<o2.getValue()){
                         return 1;
                     }else{
                         return 0;
@@ -136,10 +164,10 @@ public class HotPages {
 
             // 遍历列表，取top n输出
             for (int i = 0; i < Math.min(topN, pageViewCounts.size()); i++) {
-                PageViewCount currentItemViewCount = pageViewCounts.get(i);
+                Map.Entry<String, Long> entry = pageViewCounts.get(i);
                 resultBuilder.append("NO ").append(i + 1).append(":")
-                        .append(" 页面URL = ").append(currentItemViewCount.getUrl())
-                        .append(" 浏览量 = ").append(currentItemViewCount.getCount())
+                        .append(" 页面URL = ").append(entry.getKey())
+                        .append(" 浏览量 = ").append(entry.getValue())
                         .append("\n");
             }
             resultBuilder.append("===============================\n\n");
@@ -148,6 +176,7 @@ public class HotPages {
             Thread.sleep(1000L);
 
             out.collect(resultBuilder.toString());
+
 
         }
     }
